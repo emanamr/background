5acdebc6824918249c8d9e31216b00a696120aa1
emy amr <eng_eman5_9@hotmail.com>
2018-02-22 22:58:02 +0200

    Gallery

---
+++ back.js
@@ -0,0 +1,27 @@
+
+
+/*function myFunction(id){
+
+
+ var photo1=document.getElementById('img1');
+  var photo2=document.getElementById('img2');
+  var photo3=document.getElementById('img3');
+  var photo=document.getElementById('image')
+
+  switch (id) {
+    case img1:
+  window.image.style.background="img\almastba.com_1388747529_171.jpg";
+      break;
+      case img3:window.image.style.background="pink";
+    default:photo2=document.getElementById('image').style.background;
+
+  }
+  if(id===img1){
+photo.setAttribute("background","red");
+}
+else if(id===img2){
+  photo2=document.getElementById('image').style.background;
+}
+else {
+  document.getElementById('image').style.background=photo3;
+}}*/
